040355d44086162b22cd0988e77cdc8171310b7a
azozulya <bbbom@mail.ru>
2022-05-01 23:00:51 +0300

    init: init project

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+    },
+    entry: {
+        entry: ["./src/js/index.js", "./src/scss/index.scss"],
+        //main: path.resolve(__dirname, './src/js/index.js'),
+    },
+     output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [           
+            // CSS, PostCSS, Sass
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    //'style-loader', 
+                    //'css-loader', 
+                    //'postcss-loader', 
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            // url: false,
+                        },
+                    },       
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },     
+                ],
+            },
+             // изображения
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            // шрифты и SVG
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    plugins: [
+        //new HtmlWebpackPlugin(),
+        new ESLintPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        }),    
+        new HTMLWebpackPlugin({
+            inject: 'body',
+            template: path.resolve(__dirname, "src", "index.html"),
+            favicon: path.resolve(__dirname, "src", "favicon.ico"),
+        }),  
+    ],
+}
